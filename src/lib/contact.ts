943b744e7425e7a50228ba6e34c421a7655f47d7
Luís Santos <luis-sobral-12@hotmail.com>
2026-08-29 19:15:44 +0100

    fix: fone number

--- src/lib/contact.ts
+++ src/lib/contact.ts
@@ -1,5 +1,5 @@
 export const BRAND = "Táxis Vila da Ponte";
-export const PHONE_DISPLAY = "+351 960 102 200";
-export const PHONE_RAW = "+351960102200";
+export const PHONE_DISPLAY = "+351 965 015 688";
+export const PHONE_RAW = "+351965015688";
 
 export const MAPS_URL = "https://maps.app.goo.gl/HEKLS1a6fpPJC2ST8";
